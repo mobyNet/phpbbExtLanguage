2521df50145b81fdb944475d4a9930b9e44c93d3
mobyNet <91854411+mobyNet@users.noreply.github.com>
2021-10-16 01:41:30 +0200

    Update info_acp_bbcodeorder.php
    Update 1.0.10

--- Sitesplat/bbcodeorder/language/de/info_acp_bbcodeorder.php
+++ Sitesplat/bbcodeorder/language/de/info_acp_bbcodeorder.php
@@ -21,5 +21,4 @@ $lang = array_merge($lang, array(
 	'ACP_BBCODEORDER'				=> 'BBCodeOrder+',
 	'SS_HELPER_NOTY'	            => 'SiteSplat BBcore existiert nicht!<br />Laden Sie das <a href="http://sitesplat.com" target="_blank">BBcore</a> herunter und kopieren Sie den BBcore-Ordner in Ihren SiteSplat-Erweiterungsordner.',
 	'BBCODEORDER_NOTICE'	        => '<div class="phpinfo"><p>Die Einstellungen für diese Erweiterung sind in <strong>%1$s &#187; %2$s &#187; %3$s</strong>.</p></div>',
-));
-
+)); 
