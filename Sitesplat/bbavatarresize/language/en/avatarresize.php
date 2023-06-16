9c9fc22e63ec0572bb024e439e2b58f10808e5ac
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-16 14:53:56 +0200

    avatarresize.php en update

--- Sitesplat/bbavatarresize/language/en/avatarresize.php
+++ Sitesplat/bbavatarresize/language/en/avatarresize.php
@@ -37,10 +37,9 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge(
-	$lang, array(
-		'AVATAR_RESIZE'			=> 'Avatar Resize',
-		'AVATAR_RESIZE_EXPLAIN'	=> 'Hit the pencil button to crop your %1$s, then submit. Your %1$s will be saved with a height of %2$s pixels and a width of %3$s pixels.',
-		'BBAVATARRSIZE_NOTICE'	=> '<div class="phpinfo"><p>There is no specific configuration for this extension. After the user uploaded the avatar via UCP Avatar upload, the crop option will be available.<br /> Check the ACP &#62; Avatar settings: "Maximum avatar file size" to be 0 or 2mb or more for a smooth user experience.</p></div>',
-		'SS_HELPER_NOTY'	    => 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
-	)
-);
+$lang, array(
+'AVATAR_RESIZE'			=> 'Avatar Resize',
+'AVATAR_RESIZE_EXPLAIN'		=> 'Hit the pencil button to crop your %1$s, then submit. Your %1$s will be saved with a height of %2$s pixels and a width of %3$s pixels.',
+'BBAVATARRSIZE_NOTICE'		=> '<div class="phpinfo"><p>There is no specific configuration for this extension. After the user uploaded the avatar via UCP Avatar upload, the crop option will be available.<br /> Check the ACP &#62; Avatar settings: "Maximum avatar file size" to be 0 or 2mb or more for a smooth user experience.</p></div>',
+'SS_HELPER_NOTY'	     	=> 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
+));
