1e590d7f80282db31696ca054eacbc7cd12e6d0b
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-16 15:11:54 +0200

    bbavatarresize.php en update

--- Sitesplat/bbavatarresize/language/en/avatarresize.php
+++ Sitesplat/bbavatarresize/language/en/avatarresize.php
@@ -41,5 +41,5 @@ $lang, array(
 'AVATAR_RESIZE'		=> 'Avatar Resize',
 'AVATAR_RESIZE_EXPLAIN'	=> 'Hit the pencil button to crop your %1$s, then submit. Your %1$s will be saved with a height of %2$s pixels and a width of %3$s pixels.',
 'BBAVATARRSIZE_NOTICE'	=> '<div class="phpinfo"><p>There is no specific configuration for this extension. After the user uploaded the avatar via UCP Avatar upload, the crop option will be available.<br /> Check the ACP &#62; Avatar settings: "Maximum avatar file size" to be 0 or 2mb or more for a smooth user experience.</p></div>',
-'SS_HELPER_NOTY'	     => 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
+'SS_HELPER_NOTY'		=> 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
 ));
