2ef94a2673ea68522811a315ef95f37c9351afb1
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-17 22:23:40 +0200

    registration.php update de

--- Sitesplat/bbgdpr/language/de/registration.php
+++ Sitesplat/bbgdpr/language/de/registration.php
@@ -17,16 +17,16 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'BULK_EMAILS'				=> 'E-Mails',
-	'BULK_EMAILS_EXPLAIN'		=> 'Durch Aktivieren dieses Kontrollk&auml;stchens stimmen Sie zu, dass Administratoren dieses Boards E-Mails an Sie senden d&uuml;rfen. Die Zulassung ist erforderlich, um die Registrierung zu erm&ouml;glichen. E-Mails werden zur Aktivierung des Forums ben&ouml;tigt oder andere wichtige Sicherheits-E-Mails werden an Benutzer gesendet.',
+'BULK_EMAILS'                   => 'E-Mails',
+'BULK_EMAILS_EXPLAIN'           => 'Durch Aktivieren dieses Kontrollk&auml;stchens stimmen Sie zu, dass Administratoren dieses Boards E-Mails an Sie senden d&uuml;rfen. Die Zulassung ist erforderlich, um die Registrierung zu erm&ouml;glichen. E-Mails werden zur Aktivierung des Forums ben&ouml;tigt oder andere wichtige Sicherheits-E-Mails werden an Benutzer gesendet.',
 
-	'PRIVACY_POLICY'			=> 'Datenschutzbestimmungen',
-    'PRIVACY_POLICY_EXPLAIN'    => 'Durch Aktivieren dieses Kontrollk&auml;stchens best&auml;tigen Sie, dass Sie den Datenschutzbestimmungen dieses Forums zustimmen, die gerade auf der Seite mit den Nutzungsbedingungen angezeigt wurden. Sie finden die Datenschutzbestimmungen auch, wenn Sie sich angemeldet haben. Die Zulassung ist erforderlich, um die Registrierung zu erm&ouml;glichen.',
-	'REG_UNDRSTD'	        	=> 'Ich verstehe und stimme zu',
+'PRIVACY_POLICY'                => 'Datenschutzbestimmungen',
+'PRIVACY_POLICY_EXPLAIN'        => 'Durch Aktivieren dieses Kontrollk&auml;stchens best&auml;tigen Sie, dass Sie den Datenschutzbestimmungen dieses Forums zustimmen, die gerade auf der Seite mit den Nutzungsbedingungen angezeigt wurden. Sie finden die Datenschutzbestimmungen auch, wenn Sie sich angemeldet haben. Die Zulassung ist erforderlich, um die Registrierung zu erm&ouml;glichen.',
+'REG_UNDRSTD'                   => 'Ich verstehe und stimme zu',
 
-	'AGREEMENTS_REQUIRED'		=> 'Sie m&uuml;ssen Massen E-Mails (es handelt sich um kein Spam) und die Datenschutzbestimmungen zustimmen',
+'AGREEMENTS_REQUIRED'           => 'Sie m&uuml;ssen Massen E-Mails (es handelt sich um kein Spam) und die Datenschutzbestimmungen zustimmen',
 
-	'BULK_EMAILS_QUICK_AGREE'				=> 'E-Mails vom Forum',
-	'PRIVACY_POLICY_QUICK_AGREE'				=> 'Forum\'s GDPR',
+'BULK_EMAILS_QUICK_AGREE'       => 'E-Mails vom Forum',
+'PRIVACY_POLICY_QUICK_AGREE'    => 'Forum\'s GDPR',
 
 ));
