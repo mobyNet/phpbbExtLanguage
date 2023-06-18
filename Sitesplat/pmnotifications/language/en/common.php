9a0ea79b085afcff8ab7b50f5db41ca81fb21b1a
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-18 15:35:06 +0200

    pmnotification update en

--- Sitesplat/pmnotifications/language/en/common.php
+++ Sitesplat/pmnotifications/language/en/common.php
@@ -1,27 +1,25 @@
 <?php
 /**
-*
-* @package PMnotifications
-* @copyright (c) 2016 SiteSplat All rights reserved
-* @license Proprietary
-*
-*/
+ *
+ * @package PMnotifications
+ * @copyright (c) 2016 SiteSplat All rights reserved
+ * @license Proprietary
+ *
+ */
 
-if (!defined('IN_PHPBB'))
-{
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
 $lang = array_merge($lang, array(
-	'FROM_AUTHOR_PM_LIST'		=> 'Previous PM\'s from this user',
-	'PM_UNREAD_COUNT'	=> array(
-		0	=> 'No unread PMs. Great!',
-		1	=> '1 unread PM',
-		2	=> '%d unread PMs',
-	),
+'FROM_AUTHOR_PM_LIST'           => 'Previous PM\'s from this user',
+'PM_UNREAD_COUNT' => array(
+0                               => 'No unread PMs. Great!',
+1                               => '1 unread PM',
+2                               => '%d unread PMs',
+),
 ));
