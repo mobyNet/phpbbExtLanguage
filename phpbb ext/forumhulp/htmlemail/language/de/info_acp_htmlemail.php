22465542f63a554bcf9f11aaf8ef2d3de99cf6b2
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-19 02:10:14 +0200

    HTML email update de

--- phpbb ext/forumhulp/htmlemail/language/de/info_acp_htmlemail.php
+++ phpbb ext/forumhulp/htmlemail/language/de/info_acp_htmlemail.php
@@ -1,23 +1,21 @@
 <?php
 /**
-*
-* HTML email extension for the phpBB Forum Software package.
-*
-* @copyright (c) 2015 ForumHulp.com <http://forumhulp.com>
-* @license GNU General Public License, version 2 (GPL-2.0)
-*
-*/
+ *
+ * HTML email extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 ForumHulp.com <http://forumhulp.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
 /**
-* DO NOT CHANGE
-*/
-if (!defined('IN_PHPBB'))
-{
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
@@ -38,8 +36,8 @@ if (empty($lang) || !is_array($lang))
 //
 
 $lang = array_merge($lang, array(
-	'FH_HELPER_NOTICE'		=> 'Forumhulp helper Anwendung existiert nicht!<br />Laden Sie <a href="https://github.com/ForumHulp/helper" target="_blank">forumhulp/helper</a> herunter und kopieren Sie den helper Ordner in Ihren forumhulp Erweiterungsordner.',
-	'HTML_EMAIL_NOTICE'		=> '<div class="phpinfo"><p class="entry">Einstellungen für diese Erweiterung sind nicht notwendig.<br />Sie können sie für Emails auf der Geburtstag Erweiterung verwenden, indem Sie "use html in email" in Board Funktionen einschalten. Wenn Sie html in anderen Mails verwenden wollen, müssen Sie Messenger mitteilen, dass er html-E-Mails senden soll, indem Sie Messenger mit $messenger->set_mail_html(true) konfigurieren. Aktualisieren Sie Ihre Mail-Templates nach Belieben.</p></div>',
-	'DELETE_DATA_NOTICE'	=> '<div style="width:80%;margin:20px auto;"><p style="text-align:left;">Nicht alle Dateien werden ersetzt! Bitte ersetzen Sie die im Admin-Protokoll genannte(n) Datei(en) von Hand</p></div>',
-	'ENABLE_DATA_NOTICE'	=> '<div style="width:80%;margin:20px auto;"><p style="text-align:left;"><br />Allerdings werden nicht alle Updates ordnungsgemäß durchgeführt, weil Dateiberechtigungen oder fehlende Dateien. Siehe Admin-Log für eine Übersicht.<br /><br />Bitte aktualisieren Sie die Dateien von Hand, um alle Funktionen nutzen zu können !</p></div>'
+'FH_HELPER_NOTICE'        => 'Forumhulp helper Anwendung existiert nicht!<br />Laden Sie <a href="https://github.com/ForumHulp/helper" target="_blank">forumhulp/helper</a> herunter und kopieren Sie den helper Ordner in Ihren forumhulp Erweiterungsordner.',
+'HTML_EMAIL_NOTICE'       => '<div class="phpinfo"><p class="entry">Einstellungen für diese Erweiterung sind nicht notwendig.<br />Sie können sie für Emails auf der Geburtstag Erweiterung verwenden, indem Sie "use html in email" in Board Funktionen einschalten. Wenn Sie html in anderen Mails verwenden wollen, müssen Sie Messenger mitteilen, dass er html-E-Mails senden soll, indem Sie Messenger mit $messenger->set_mail_html(true) konfigurieren. Aktualisieren Sie Ihre Mail-Templates nach Belieben.</p></div>',
+'DELETE_DATA_NOTICE'      => '<div style="width:80%;margin:20px auto;"><p style="text-align:left;">Nicht alle Dateien werden ersetzt! Bitte ersetzen Sie die im Admin-Protokoll genannte(n) Datei(en) von Hand</p></div>',
+'ENABLE_DATA_NOTICE'      => '<div style="width:80%;margin:20px auto;"><p style="text-align:left;"><br />Allerdings werden nicht alle Updates ordnungsgemäß durchgeführt, weil Dateiberechtigungen oder fehlende Dateien. Siehe Admin-Log für eine Übersicht.<br /><br />Bitte aktualisieren Sie die Dateien von Hand, um alle Funktionen nutzen zu können !</p></div>'
 ));
