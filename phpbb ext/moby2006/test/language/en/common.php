11d74606a2fa6da0f18fefabbaf625befb375746
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-19 02:23:09 +0200

    moby2006  update  all

--- phpbb ext/moby2006/test/language/en/common.php
+++ phpbb ext/moby2006/test/language/en/common.php
@@ -1,34 +1,32 @@
 <?php
 /**
-*
-* @package phpBB Extension - Acme Demo
-* @copyright (c) 2013 phpBB Group
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package phpBB Extension - Acme Demo
+ * @copyright (c) 2013 phpBB Group
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
-if (!defined('IN_PHPBB'))
-{
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
 $lang = array_merge($lang, array(
-	'DEMO_PAGE'			=> 'Demo',
-	'DEMO_HELLO'		=> 'Hallo %s!',
-	'DEMO_GOODBYE'		=> 'Goodbye %s!',
+'DEMO_PAGE'                   => 'Demo',
+'DEMO_HELLO'                  => 'Hallo %s!',
+'DEMO_GOODBYE'                => 'Goodbye %s!',
 
-	'ACP_TEST_TITLE'            => 'Test',
-    'ACP_TEST'                  => 'Translation_de',
-    'ACP_TEST2'                  => 'Test2',
-    'ACP_TEST3'                  => 'Test3',
-    'ACP_TEST4'                  => 'Test4',
-    
-    'ACP_DEMO_TITLE'			=> 'Translation Tool Ext',
-	'ACP_DEMO_GOODBYE'			=> 'Wollen Sie Goodbye sagen?',
-	'ACP_DEMO_SETTING_SAVED'	=> 'Die Einstellungen wurden gespeichert!',
+'ACP_TEST_TITLE'              => 'Test',
+'ACP_TEST'                    => 'Translation_de',
+'ACP_TEST2'                   => 'Test2',
+'ACP_TEST3'                   => 'Test3',
+'ACP_TEST4'                   => 'Test4',
+
+'ACP_DEMO_TITLE'              => 'Translation Tool Ext',
+'ACP_DEMO_GOODBYE'            => 'Wollen Sie Goodbye sagen?',
+'ACP_DEMO_SETTING_SAVED'      => 'Die Einstellungen wurden gespeichert!',
 ));
