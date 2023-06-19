ebf5ab108a103c67c5152fe3cab14c87a91dc0c4
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-19 02:15:08 +0200

    PM Welcome update en

--- phpbb ext/leinad4mind/pmwelcome/language/en/acp/info_acp_pmwelcome.php
+++ phpbb ext/leinad4mind/pmwelcome/language/en/acp/info_acp_pmwelcome.php
@@ -34,23 +34,23 @@ if (empty($lang) || !is_array($lang)) {
 
 
 $lang = array_merge($lang, [
-   // General config options
-   'ACP_PMWELCOME'               => 'Welcome message',
-   'ACP_PMWELCOME_EXPLAIN'         => 'You can specify the text of the personal message that will be sent to the user upon registration.',
-   'ACP_PMWELCOME_SETTINGS'      => 'Settings private message welcome',
-   'ACP_PMWELCOME_USER'         => 'Sender',
-   'ACP_PMWELCOME_USER_EXPLAIN'   => 'The user, on behalf of which, will be sent the private message to the new board member.',
-   'ACP_PMWELCOME_SUBJECT'         => 'Welcome subject',
-   'ACP_PMWELCOME_SUBJECT_SHORT'         => 'Subject',
-   'ACP_PMWELCOME_TEXT'         => 'Text of the welcome message',
-   'ACP_PMWELCOME_TEXT_SHORT'         => 'Text',
-   'ACP_PMWELCOME_TEXT_EXPLAIN'   => 'You can use bbcode and smilies, and the following tokens: {USERNAME} to replace the name of the user who receives a private message; {USER_EMAIL} to replace the email of the user who receives a private message; {SITE_NAME} and {SITE_DESC} to replace with the name and description of your forum; {BOARD_CONTACT}, {BOARD_EMAIL} and {BOARD_SIG} to replace the forum contact, email and signature; As well as {SENDER} to insert the senders name.',
-   'ACP_PMWELCOME_PREVIEW'         => 'PM Welcome - Preview',
-   'ACP_PMWELCOME_NO_USER'         => '<b>The requested sender doesn’t exist</b>',
-   'ACP_PMWELCOME_CONFIG_SAVED'   => 'PM Welcome config was saved',
-   'TOO_SHORT_PMWELCOME_SUBJECT'   => 'The value for the welcome subject is too short',
-   'TOO_SHORT_PMWELCOME_POST_TEXT'   => 'The value for the private message welcome text is too short',
-   // Log entries
-   'LOG_CONFIG_PMWELCOME_ADMIN'      => '<strong>Altered private message welcome extension page settings</strong>',
-   'LOG_PMWELCOME_CONFIG_UPDATE'      => '<strong>Updated Private message welcome config settings</strong>',
+// General config options
+'ACP_PMWELCOME'                        => 'Welcome message',
+'ACP_PMWELCOME_EXPLAIN'                => 'You can specify the text of the personal message that will be sent to the user upon registration.',
+'ACP_PMWELCOME_SETTINGS'               => 'Settings private message welcome',
+'ACP_PMWELCOME_USER'                   => 'Sender',
+'ACP_PMWELCOME_USER_EXPLAIN'           => 'The user, on behalf of which, will be sent the private message to the new board member.',
+'ACP_PMWELCOME_SUBJECT'                => 'Welcome subject',
+'ACP_PMWELCOME_SUBJECT_SHORT'          => 'Subject',
+'ACP_PMWELCOME_TEXT'                   => 'Text of the welcome message',
+'ACP_PMWELCOME_TEXT_SHORT'             => 'Text',
+'ACP_PMWELCOME_TEXT_EXPLAIN'           => 'You can use bbcode and smilies, and the following tokens: {USERNAME} to replace the name of the user who receives a private message; {USER_EMAIL} to replace the email of the user who receives a private message; {SITE_NAME} and {SITE_DESC} to replace with the name and description of your forum; {BOARD_CONTACT}, {BOARD_EMAIL} and {BOARD_SIG} to replace the forum contact, email and signature; As well as {SENDER} to insert the senders name.',
+'ACP_PMWELCOME_PREVIEW'                => 'PM Welcome - Preview',
+'ACP_PMWELCOME_NO_USER'                => '<b>The requested sender doesn’t exist</b>',
+'ACP_PMWELCOME_CONFIG_SAVED'           => 'PM Welcome config was saved',
+'TOO_SHORT_PMWELCOME_SUBJECT'          => 'The value for the welcome subject is too short',
+'TOO_SHORT_PMWELCOME_POST_TEXT'        => 'The value for the private message welcome text is too short',
+// Log entries
+'LOG_CONFIG_PMWELCOME_ADMIN'           => '<strong>Altered private message welcome extension page settings</strong>',
+'LOG_PMWELCOME_CONFIG_UPDATE'          => '<strong>Updated Private message welcome config settings</strong>',
 ]);
