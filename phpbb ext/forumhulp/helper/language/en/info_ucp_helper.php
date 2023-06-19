c1b2b7d79d78d3ad53899e0a9deaa50fae214cb3
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-19 02:08:13 +0200

    Forumhulp Helper update en

--- phpbb ext/forumhulp/helper/language/en/info_ucp_helper.php
+++ phpbb ext/forumhulp/helper/language/en/info_ucp_helper.php
@@ -1,22 +1,20 @@
 <?php
 /**
-*
-* @package Forumhulp Helper
-* @copyright (c) 2015 ForumHulp.com
-* @license Proprietary
-*
-*/
+ *
+ * @package Forumhulp Helper
+ * @copyright (c) 2015 ForumHulp.com
+ * @license Proprietary
+ *
+ */
 
 /**
  * DO NOT CHANGE
  */
-if (!defined('IN_PHPBB'))
-{
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
@@ -36,16 +34,13 @@ if (empty($lang) || !is_array($lang))
 // ’ » “ ” …
 //
 
-$lang = array_merge(
-	$lang, array(
-		'UCP_IR'				=> 'Image Resizer',
-		'IMAGE_RESIZE'			=> 'Image Resize',
-		'IMAGE_RESIZE_EXPLAIN'	=> 'Crop your %1$s from the picture and confirm.',
-		// Your avatar will be saved with a height of %1$s pixels and a width of %2$s pixels.',
-
-		'IMAGE_RESIZE_ERROR'	=> 'No app or file to send!',
-		'IMAGERESIZE_NOTICE'	=> '<div class="phpinfo"><p class="entry">This module is hidden and has no config options. However remind you to set maximum upload in ACP.</p></div>',
-		'ERROR_IMAGERESIZE_DISABLE'	=> 'You can not disable Image Resize because of active extensions<br /><div class="phpinfo">» %s</div>',
-	'FH_HELPER_NOTICE'				=> 'Forumhulp helper application does not exist!<br />Download <a href="https://github.com/ForumHulp/helper" target="_blank">forumhulp/helper</a> and copy the helper folder to your forumhulp extension folder.',
-	)
-);
+$lang = array_merge($lang,array(
+'UCP_IR'                          => 'Image Resizer',
+'IMAGE_RESIZE'                    => 'Image Resize',
+'IMAGE_RESIZE_EXPLAIN'            => 'Crop your %1$s from the picture and confirm.',
+// Your avatar will be saved with a height of %1$s pixels and a width of %2$s pixels.'
+'IMAGE_RESIZE_ERROR'              => 'No app or file to send!',
+'IMAGERESIZE_NOTICE'              => '<div class="phpinfo"><p class="entry">This module is hidden and has no config options. However remind you to set maximum upload in ACP.</p></div>',
+'ERROR_IMAGERESIZE_DISABLE'       => 'You can not disable Image Resize because of active extensions<br /><div class="phpinfo">» %s</div>',
+'FH_HELPER_NOTICE'                => 'Forumhulp helper application does not exist!<br />Download <a href="https://github.com/ForumHulp/helper" target="_blank">forumhulp/helper</a> and copy the helper folder to your forumhulp extension folder.',
+));
