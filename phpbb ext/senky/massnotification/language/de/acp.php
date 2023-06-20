dbe1692b516e1648eda582e92c1f9b637c69aaf0
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-20 23:26:18 +0200

    massnotification update de

--- phpbb ext/senky/massnotification/language/de/acp.php
+++ phpbb ext/senky/massnotification/language/de/acp.php
@@ -8,34 +8,32 @@
  *
  */
 
-if (!defined('IN_PHPBB'))
-{
+if (!defined('IN_PHPBB')) {
 	exit;
 }
 
-if (empty($lang) || !is_array($lang))
-{
+if (empty($lang) || !is_array($lang)) {
 	$lang = array();
 }
 
 $lang = array_merge($lang, array(
-	'MASSNOTIFICATION_DESC'	=> 'Hier können Sie eine Board-Benachrichtigung entweder an alle Ihre Benutzer oder an bestimmte Benutzer oder Gruppen senden.',
+'MASSNOTIFICATION_DESC'              => 'Hier können Sie eine Board-Benachrichtigung entweder an alle Ihre Benutzer oder an bestimmte Benutzer oder Gruppen senden.',
 
-	'COMPOSE'				=> 'Massenbenachrichtigung verfassen',
-	'ALL_USERS'				=> 'Alle Benutzer',
-	'SEND_TO_GROUP'			=> 'An Gruppe senden',
-	'SEND_TO_USERS'			=> 'An Benutzer senden',
-	'SEND_TO_USERS_EXPLAIN'	=> 'Wenn Sie hier einen Namen eingeben, wird die oben ausgewählte Gruppe überschrieben. Geben Sie jeden Benutzernamen in eine neue Zeile ein.',
-	'TITLE'					=> 'Titel der Benachrichtigung',
-	'MESSAGE_EXPLAIN'		=> 'Fassen Sie sich kurz, vorzugsweise unter 150 Zeichen. Sie können HTML verwenden.',
-	'URL'					=> 'Weiterleitungs-URL',
-	'URL_EXPLAIN'			=> 'Geben Sie die URL an, zu der der Benutzer nach dem Anklicken der Benachrichtigung weitergeleitet wird.',
-	'AUTHOR_EXPLAIN'		=> 'Geben Sie den Autor der Benachrichtigung an. Leer lassen für generische Benachrichtigung ohne Autor.',
-	'SEND_NOTIFICATION'		=> 'Benachrichtigung senden',
+'COMPOSE'                            => 'Massenbenachrichtigung verfassen',
+'ALL_USERS'                          => 'Alle Benutzer',
+'SEND_TO_GROUP'                      => 'An Gruppe senden',
+'SEND_TO_USERS'                      => 'An Benutzer senden',
+'SEND_TO_USERS_EXPLAIN'              => 'Wenn Sie hier einen Namen eingeben, wird die oben ausgewählte Gruppe überschrieben. Geben Sie jeden Benutzernamen in eine neue Zeile ein.',
+'TITLE'                              => 'Titel der Benachrichtigung',
+'MESSAGE_EXPLAIN'                    => 'Fassen Sie sich kurz, vorzugsweise unter 150 Zeichen. Sie können HTML verwenden.',
+'URL'                                => 'Weiterleitungs-URL',
+'URL_EXPLAIN'                        => 'Geben Sie die URL an, zu der der Benutzer nach dem Anklicken der Benachrichtigung weitergeleitet wird.',
+'AUTHOR_EXPLAIN'                     => 'Geben Sie den Autor der Benachrichtigung an. Leer lassen für generische Benachrichtigung ohne Autor.',
+'SEND_NOTIFICATION'                  => 'Benachrichtigung senden',
 
-	'NO_NOTIFICATION_TITLE'		=> 'Sie müssen den Titel der Meldung angeben.',
-	'NO_NOTIFICATION_MESSAGE'	=> 'Sie müssen die Nachricht der Benachrichtigung angeben.',
-	'NO_NOTIFICATION_AUTHOR'	=> 'Autor existiert nicht.',
+'NO_NOTIFICATION_TITLE'              => 'Sie müssen den Titel der Meldung angeben.',
+'NO_NOTIFICATION_MESSAGE'            => 'Sie müssen die Nachricht der Benachrichtigung angeben.',
+'NO_NOTIFICATION_AUTHOR'             => 'Autor existiert nicht.',
 
-	'NOTIFICATION_SEND'	=> 'Ihre Benachrichtigung wurde gesendet.',
+'NOTIFICATION_SEND'                  => 'Ihre Benachrichtigung wurde gesendet.',
 ));
--- phpbb ext/senky/massnotification/language/de/info_acp_demo.php
+++ phpbb ext/senky/massnotification/language/de/info_acp_demo.php
@@ -19,5 +19,5 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_SENKY_MASSNOTIFICATION_TITLE'	=> 'Massenbenachrichtigung',
+'ACP_SENKY_MASSNOTIFICATION_TITLE'       => 'Massenbenachrichtigung',
 ));
