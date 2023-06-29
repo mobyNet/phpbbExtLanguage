b8c2e7aedee7881812b85f5e2a4e1f9af8308f74
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-29 19:09:41 +0200

    bbembed de/en complete

--- Sitesplat/bbembed/language/en/info_acp_bbembed.php
+++ Sitesplat/bbembed/language/en/info_acp_bbembed.php
@@ -18,7 +18,7 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_BBEMBED'       => 'BBembed',
-	'SS_HELPER_NOTY'    => 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
-	'BBEMBED_NOTICE'    => '<div class="phpinfo"><p>There are no specific settings for this extension. Post straight links into posts and BBembed will automagically display the videos.</p></div>',
+'ACP_BBEMBED'       => 'BBembed',
+'SS_HELPER_NOTY'    => 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
+'BBEMBED_NOTICE'    => '<div class="phpinfo"><p>There are no specific settings for this extension. Post straight links into posts and BBembed will automagically display the videos.</p></div>',
 ));
