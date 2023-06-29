ea5841e09c3f1128f85e8e5d6bf2e4a915164344
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-29 19:06:33 +0200

    bbcodeorder de/en complete

--- Sitesplat/bbcodeorder/language/en/info_acp_bbcodeorder.php
+++ Sitesplat/bbcodeorder/language/en/info_acp_bbcodeorder.php
@@ -18,7 +18,7 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_BBCODEORDER'      => 'BBCodeOrder+',
-	'SS_HELPER_NOTY'       => 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
-	'BBCODEORDER_NOTICE'   => '<div class="phpinfo"><p>The Setting for this extension are in <strong>%1$s &#187; %2$s &#187; %3$s</strong>.</p></div>',
+'ACP_BBCODEORDER'      => 'BBCodeOrder+',
+'SS_HELPER_NOTY'       => 'SiteSplat BBcore does not exist!<br />Download the <a href="https://sitesplat.com" target="_blank">BBcore</a> and copy the BBcore folder into your sitesplat extension folder.',
+'BBCODEORDER_NOTICE'   => '<div class="phpinfo"><p>The Setting for this extension are in <strong>%1$s &#187; %2$s &#187; %3$s</strong>.</p></div>',
 )); 
