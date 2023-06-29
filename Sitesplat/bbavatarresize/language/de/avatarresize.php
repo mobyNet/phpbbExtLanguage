18b32d4e37148eeecb6a96129b12c6be26a131ee
Hans Dieter Feil <mail@h-d-feil.de>
2023-06-29 19:02:40 +0200

    avatarresize de/en complete

--- Sitesplat/bbavatarresize/language/de/avatarresize.php
+++ Sitesplat/bbavatarresize/language/de/avatarresize.php
@@ -36,8 +36,7 @@ if (empty($lang) || !is_array($lang))
 // ’ » “ ” …
 //
 
-$lang = array_merge(
-$lang, array(
+$lang = array_merge($lang, array(
 'AVATAR_RESIZE' 				=> 'Avatar Größe ändern',
 'AVATAR_RESIZE_EXPLAIN'			=> 'Klicke auf bearbeiten, um  das %1$s zuzuschneiden, und Sende es ab. Ihr %1$s wird mit einer Höhe von %2$s Pixel und einer Breite von %3$s Pixel gespeichert.',
 'BBAVATARRSIZE_NOTICE'			=> '<div class="phpinfo"><p>Für diese Erweiterung gibt es keine spezifische Konfiguration. Nachdem der Benutzer den Avatar über den UCP Avatar-Upload hochgeladen hat, ist die Zuschneideoption verfügbar.<br /> &Uuml;berpr&uuml;fe im ACP &#62; Avatar Einstellungen: "Avatar-Dateigröße": 0 oder 2 MB oder mehr für eine reibungslose Benutzererfahrung.</p></div>',
